a48e323821071cbb83644aa95e54b7f50c1af877
ka1te6 <a.andrey2006@yandex.ru>
2025-03-14 02:21:18 +0300

    Изменён язык коммита

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
-
-int
-main()
-{
+#include <iostream> // подключено для работы с вводом/выводом
+// я уже убрал это ранее, поэтому просто оставлю комментарий
+int main(){
   std::string name;
   std::cin >> name;
   std::cout << "Hello, world from " << name << "!" << std::endl;
